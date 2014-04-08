4f1a6e1014d2cac85e01814b488b0f549cc48204
Jamie Hoyle <j@jamiehoyle.com>
2014-04-08 23:04:48 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,76 @@
+//source: http://stackoverflow.com/questions/8498592/extract-root-domain-name-from-string
+function parseURL(url){
+    parsed_url = {}
+
+    if ( url == null || url.length == 0 )
+        return parsed_url;
+
+    protocol_i = url.indexOf('://');
+    parsed_url.protocol = url.substr(0,protocol_i);
+
+    remaining_url = url.substr(protocol_i + 3, url.length);
+    domain_i = remaining_url.indexOf('/');
+    domain_i = domain_i == -1 ? remaining_url.length - 1 : domain_i;
+    parsed_url.domain = remaining_url.substr(0, domain_i);
+    parsed_url.path = domain_i == -1 || domain_i + 1 == remaining_url.length ? null : remaining_url.substr(domain_i + 1, remaining_url.length);
+
+    domain_parts = parsed_url.domain.split('.');
+    switch ( domain_parts.length ){
+        case 2:
+          parsed_url.subdomain = null;
+          parsed_url.host = domain_parts[0];
+          parsed_url.tld = domain_parts[1];
+          break;
+        case 3:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2];
+          break;
+        case 4:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2] + '.' + domain_parts[3];
+          break;
+    }
+
+    parsed_url.parent_domain = parsed_url.host + '.' + parsed_url.tld;
+
+    return parsed_url;
+}
+
+// background script for access to Chrome APIs
+chrome.tabs.onUpdated.addListener(function(tabId, info) {
+    //check page has loaded
+    if (info.status === 'complete') {
+        //get the tab's URL
+        chrome.tabs.getSelected(null, function(tab) {
+          var currentURL = tab.url;
+          var parsedURL = parseURL(currentURL);
+          //Google, bit.ly, t.co (and other URL shortners) do some funny URL things, we want to stop it, ergo reducing requests to the server
+          //if(parsedURL.domain.indexOf("google") == -1 || parsedURL.domain.indexOf("bit.ly") == -1 || parsedURL.domain.indexOf("t.co") == -1 || parsedURL.domain.indexOf("buff.ly") == -1) {
+            //doesn't contain any of the above, carry on
+            var bleedURL = 'http://bleed-1161785939.us-east-1.elb.amazonaws.com/bleed/' + parsedURL.domain;
+            promise.get(bleedURL).then(function(error, text, xhr) {
+              if (error) {
+                //silently fail
+                return;
+              } else {
+                //parse as JSON, check result
+                var result = JSON.parse(text);
+                console.log('Result for site ' + bleedURL + ': ' + result.code);
+                if(result.code === 0) {
+                  var notification = webkitNotifications.createNotification(
+                    'logo.png',  // icon url - can be relative
+                    'This site is vulnerable!',  // notification title
+                    'The domain ' + parsedURL.domain + ' is vulnerable to the Heartbleed SSL bug.'  // notification body text
+                  );
+                  notification.show();
+                } else {
+                  //do nothing
+                  return;
+                }
+              }
+            });
+        });
+    }
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+#Chromebleed
+Displays a warning if the site you are browsing is affected by the Heartbleed bug.  
+
+##What does this do?
+Many HTTPS-secured sites on the internet use OpenSSL. Unfortunately, a major vulnerability in OpenSSL was disclosed - known as the Heartbleed bug - yesterday that put hundreds of thousands of servers at risk of compromise.  
+
+Whilst some servers have been patched already, many remain that have not been patched. Chromebleed uses a web service developed by Filippo Valsorda and checks the URL of the page you have just loaded. If it is affected by Heartbleed, then a Chrome notification will be displayed. It's as simple as that!  
+
+**Beware of gremlins** - this was developed in about half an hour rather late at night. Bugs will exist somewhere!  
+
+##Download  
+https://chrome.google.com/webstore/detail/chromebleed/eeoekjnjgppnaegdjbcafdggilajhpic
