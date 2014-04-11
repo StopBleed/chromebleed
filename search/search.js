69897f49b3734d5eb54d08d5b5c2522d9cf53a20
talves <tony@3alves.com>
2014-04-11 13:08:11 -0700

    Cleanup search to use cache Still needs some fixing...

--- search/search.js
+++ search/search.js
@@ -1,85 +1,91 @@
 function checkURL(host, tld, parsedDomain) {
-  var url = host + '.' + tld;
-  requestURL(parsedDomain, function(text) {
-    var result = JSON.parse(text);
-    //register it as checked
-    if(result.code === 0) {
-      //it is vuln.
-      $('.' + host + tld).prepend('<img class="chromebleedError" src="http://jamiehoyle.com/icon128.png" alt="Chromebleed Checker" />');
-      return 'fail';
-    } else {
-      //add to the 'okay' register
-      isokay.push(parsedDomain);
-      while (isokay.length > 250) {
-          isokay.shift();
-      }
-      localStorage.isokay = JSON.stringify(isokay);
-    }
-  })
+    var url = host + '.' + tld;
+    requestURL(parsedDomain, function(text) {
+        var result = JSON.parse(text);
+        console.log("  Returned: " + text);
+        //register it as checked
+        if (result.code === 0) {
+            //it is vuln.
+            console.log("  Vuln Site: " + parsedDomain);
+            if (!isCachedBleedSite(parsedDomain)) {
+                // Only need to add once for this domain
+                cacheBleedSite(parsedDomain);
+                $('.' + host + tld).prepend('<img class="chromebleedError" src="https://raw.githubusercontent.com/hyl/chromebleed/master/icon128.png" alt="Chromebleed Checker" />');
+            }
+            return 'fail';
+        } else {
+            //add to the cached sites
+            console.log("  Adding Site to Cache: " + parsedDomain);
+            cacheSite(parsedDomain);
+        }
+    });
 }
 
 function checkElements() {
-  //set the eid
-  eid = $('#rso').attr('eid');
-  //and search text
-  searchText = $('#gbqfq').val();
-  //iterate
-  $('h3 a').each(function() {
-    var alreadyChecked = 0;
-    var checkingURL = ($(this).attr('href'));
-    var parsedURL = parseURL(checkingURL);
-    $(this).attr('class', parsedURL.host + parsedURL.tld);
-    //check if already checked
-    if($(this).attr('checkedByChromebleed')) {
-      alreadyChecked = 1;
-    } else {
-      //check if in last 250 URLs
-      isokay.some(function(site) {
-          if (parsedURL.domain == site) {
-              alreadyChecked = 1;
-              return true;
-          }
-      });
-    }
-    //now check alreadyChecked
-    if(alreadyChecked === 0) {
-      //it's going to be checked
-      $('.' + parsedURL.host + parsedURL.tld).attr('checkedByChromebleed', '1');
-      checkURL(parsedURL.host, parsedURL.tld, parsedURL.domain);
-    }
-  })
+    //In search we only cache bleed sites for the current search
+    resetCachedBleedSites();
+    //set the eid
+    eid = $('#rso').attr('eid');
+    //and search text
+    searchText = $('#gbqfq').val();
+    //iterate
+    $('h3 a').each(function() {
+        var alreadyChecked = false;
+        var checkingURL = ($(this).attr('href'));
+        console.log("  Checking Url: " + checkingURL);
+        var parsedURL = parseURL(checkingURL);
+        console.log("  Parsed to: " + JSON.stringify(parsedURL));
+        $(this).attr('class', parsedURL.host + parsedURL.tld);
+        //check if already checked
+        if ($(this).attr('checkedByChromebleed')) {
+            console.log("  Checked OnPage: " + checkingURL);
+            // marked as checked on this page
+            alreadyChecked = true;
+        } else {
+            //check if in cached sites
+            alreadyChecked = isCachedSite(parsedURL.domain);
+        }
+        console.log((alreadyChecked ? "  alreadyChecked: " : "  NOT alreadyChecked: ") + parsedURL.domain);
+        //now check alreadyChecked
+        if (!alreadyChecked && !isCachedBleedSite(parsedURL.domain)) {
+            //it's going to be checked for a Bleed Site
+            $('.' + parsedURL.host + parsedURL.tld).attr('checkedByChromebleed', '1');
+            checkURL(parsedURL.host, parsedURL.tld, parsedURL.domain);
+        }
+    });
 }
 
 function waitForLoad() {
 //wait for results to display
-  if($('.g').length > 0) {
-    //elements have arrived
-    checkElements();
-  } else {
-    setTimeout(waitForLoad, 500);
-  }
+    if ($('.g').length > 0) {
+        //elements have arrived
+        checkElements();
+    } else {
+        setTimeout(waitForLoad, 500);
+    }
 }
 
 function searchUpdate() {
-  newEid = $('#rso').attr('eid');
-  newSearchText = $('#gbqfq').val();
-  if(newEid === eid) {
-    setTimeout(searchUpdate, 500);
-  } else if(newSearchText === searchText) {
-  } else {
-    checkElements();
-  }
+    newEid = $('#rso').attr('eid');
+    newSearchText = $('#gbqfq').val();
+    if (newEid === eid) {
+        setTimeout(searchUpdate, 500);
+    } else if (newSearchText === searchText) {
+    } else {
+        checkElements();
+    }
 }
 
 //upon page load, check page elements
 $(document).ready(function() {
-  checkElements();
+    console.log("Checking Elements ----->");
+    checkElements();
 });
 
 $('#gbqfb').click(function() {
-  searchUpdate();
-})
+    searchUpdate();
+});
 
 $('#gbqfsa').click(function() {
-  waitForLoad();
-})
+    waitForLoad();
+});
